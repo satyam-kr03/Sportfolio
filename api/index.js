151f7a9a6181377be60b52567e06d74c93a9cdae
satyam-kr03 <b22236@students.iitmandi.ac.in>
2024-06-08 23:47:55 +0530

    starter setup with auth

---
+++ api/index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const User = require("./models/User.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "dsfpa4254sdf";
+const CookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(CookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json({ error: e });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        {
+          email: userDoc.email,
+          id: userDoc._id,
+          name: userDoc.name,
+        },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
+    } else {
+      res.status(422).json("pass not ok");
+    }
+  } else {
+    res.json("not found");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+
+app.listen(4000);
